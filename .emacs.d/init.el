63eb7041bc58a916290186fc5608aa06f6a523e9
rizwan <rizwan@synopsys.com>
2023-10-03 10:32:39 +0200

    remapped C-x C-c keys and removed God-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,179 +1,115 @@
+;; Set up package.el to work with MELPA
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
-;; Comment/uncomment this line to enable MELsPA Stable if desired.
-;; See `package-archive-priorities` and `package-pinned-packages`.
-;; Most users will not need nor want to do this.
-;; (add-to-list 'package-archives
-;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 (unless package-archive-contents
-  (package-referesh-contents))
+  (package-refresh-contents))
 
-;; reduce startup time
+;; set username
+(setq user-full-name "rizwan ahmed afzal")
+(setq user-mail-address "rizwan@synopsys.com")
+
+;; ask y or n instead of yes or no
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; speed up emacs startup
 ;; The default is 800 kilobytes.  Measured in bytes.
 (setq gc-cons-threshold (* 50 1000 1000))
 
-;; initialise use-package
+;; Remove useless whitespace before saving a file
+(add-hook 'before-save-hook 'whitespace-cleanup)
+(add-hook 'before-save-hook (lambda() (delete-trailing-whitespace)))
+
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; set username and email-id
-(setq user-full-name "rizwan ahmed afzal")
-(setq user-mail-address "riz.ahmed@gmx.de")
-
-;; reduce typing by prompts
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; remove useless whitespaces while saving a file
-(add-hook 'before-save-hook 'whitespace-cleanup)
-(add-hook 'before-save-hook (lambda () (delete-trailing-whitespace)))
-
-;; for better startup performace
-(setq gc-cons-threshold (* 50 1000 1000))
-
-;; god-mode (no more RSI)
-(setq god-mode-enable-function-key-translation nil)
-(require 'god-mode)
-(god-mode)
-(global-set-key (kbd "C-;") #'god-mode-all)
-(setq god-exempt-major-modes nil)
-(setq god-exempt-predicates nil)
-
-(defun my-god-mode-toggle-on-overwrite () ;; pause god-mode when in overwrite mode
-  "Toggle god-mode on overwrite-mode."
-  (if (bound-and-true-p overwrite-mode)
-      (god-local-mode-pause)
-    (god-local-mode-resume)))
-
-(add-hook 'overwrite-mode-hook #'my-god-mode-toggle-on-overwrite)
-
-(require 'god-mode-isearch)             ;; god-mode in i-search as well
-(define-key isearch-mode-map (kbd "C-;") #'god-mode-isearch-activate)
-(define-key god-mode-isearch-map (kbd "C-;") #'god-mode-isearch-disable)
-
-(define-key god-local-mode-map (kbd ".") #'repeat) ;; vim like dot command for repeating previous action
-(define-key god-local-mode-map (kbd "i") #'god-local-mode) ;; insert into god-mode in a lcoal buffer
-
-(add-to-list 'god-exempt-major-modes 'dired-mode) ;; exempt god-mode in dired buffer
-
-(global-set-key (kbd "C-x C-1") #'delete-other-windows)
-(global-set-key (kbd "C-x C-2") #'split-window-below)
-(global-set-key (kbd "C-x C-3") #'split-window-right)
-(global-set-key (kbd "C-x C-0") #'delete-window)
-
-(define-key god-local-mode-map (kbd "[") #'backward-paragraph)
-(define-key god-local-mode-map (kbd "]") #'forward-paragraph)
-
-(custom-set-faces
- '(god-mode-lighter ((t (:inherit error)))))
-
-(defun my-god-mode-update-cursor-type ()
-  (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
-
-(add-hook 'post-command-hook #'my-god-mode-update-cursor-type)
-
-;; auto-suggestions / completions in the mini-buffer
-(require 'ido)
-(ido-mode t)
-(ido-everywhere 1)
-
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-;; (global-set-key (kbd "<escape>") 'kbd-escape-quit)
-
+;; inhibit the startup screen
+(setq inhibit-startup-screen t)
+;;
 ;; remove all gui
-(tool-bar-mode -1)	;removing clickable toolstrips
+(menu-bar-mode -1)
+(tool-bar-mode -1)
 (scroll-bar-mode -1)
 (tooltip-mode -1)
-(set-fringe-mode 10)
-(menu-bar-mode -1)
+(set-fringe-mode -1)
 (show-paren-mode 1)
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "Iosevka" :height 150)
+;; c-style comments
+(add-hook 'c-mode-hook (lambda ()
+                         (interactive)
+                         (c-toggle-comment-style -1)))
 
-;; modeline
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode t))
+;; set font size
+(set-face-attribute 'default nil :font "Iosevka" :height 140)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "991ca4dbb23cab4f45c1463c187ac80de9e6a718edc8640003892a2523cb6259" "251ed7ecd97af314cd77b07359a09da12dcd97be35e3ab761d4a92d8d8cf9a71" "636b135e4b7c86ac41375da39ade929e2bd6439de8901f53f88fde7dd5ac3561" "2e05569868dc11a52b08926b4c1a27da77580daa9321773d92822f7a639956ce" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
- '(package-selected-packages
-   '(god-mode vterm yasnippet dired-ranger dired-range dired-single general which-key python-mode multiple-cursors magit doom-modeline doom-themes zenburn-theme lsp-mode tree-sitter-langs tree-sitter org-roam-ui move-text all-the-icons-dired org-roam org-bullets use-package rust-mode company mu4e smex gruber-darker-theme)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; useful global keymaps
+(global-set-key (kbd "C-<down>") (kbd "C-u 1 C-v")) ;; scroll up
+(global-set-key (kbd "C-<up>") (kbd "C-u 1 M-v"))   ;; scroll down
 
-;; active theme
-(load-theme 'doom-palenight t)
+(define-key key-translation-map (kbd "C-;") (kbd "C-x"))
+(define-key key-translation-map (kbd "C-.") (kbd "C-c"))
 
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
+;; set a theme
+(load-theme 'zenburn t)
 
 ;; line numbers
 (column-number-mode)
 (global-display-line-numbers-mode t)
 (setq display-line-numbers-type 'relative)
 
-;; display the colored shell output properly without dispalying any wired symbols
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+;; completions
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
 
-;; enable autopair minor mode by default
+;; display colored shell properly without any wiered symbols
+(add-hook 'shell-mode-hook 'ansi-color=for-comint-mode-on)
+
+;; enable minibuffer completion
+;; (setq fido-mode t)
+;; (setq icomplete-vertical-mode t)
+
+;; start the initial frame maximized (start emacs window maximised)
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+;; enabble globbing in shell-mode
+(setq dired-enable-globstar-in-shell t)
+
+;; don't ask questions while deleting
+(setq dired-recursive-deletes 'always)
+
+;; enable autopairs by default
 (electric-pair-mode 1)
-
-;; enable globbing in shell-mode
-(setq dired-enable-globstar-in-shett t)
 
 ;; disable emacs to automatically save files
 (setq make-backup-files nil)
 (setq backup-inhibited t)
 (setq auto-save-default nil)
 
-;; save backup fiels in a dedicated dir
+;; Save backup files in a dedicated directory
 (setq backup-directory-alist '(("." . "~/.saves")))
 
-;; set locale to UTF8
+;; Set locale to UTF8
+(set-language-environment 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
 
-;; remember and restore last cursor location in the file edited recently
+;; remeber and restrore the last cursor location in the file edited recently
 (save-place-mode 1)
 
 ;; disable UI pop-up dialoge box to keep emacs fully keyboard driven
 (setq use-dialog-box nil)
 
-;; automatically referesh files when underlying buffers have changed
+;; automatically referesh files when the underlying buffers have changed (useful when buffers are changed by other editors )
 (global-auto-revert-mode 1)
 
-;; automatically referesh non-file buffers (such as dired on changes)
+;; do the same on non-file buffers (such as dired)
 (setq global-auto-revert-non-file-buffers t)
-
-;;; multiple cursors
-(require 'multiple-cursors)
-
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->")         'mc/mark-next-like-this)
-(global-set-key (kbd "C-<")         'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<")     'mc/mark-all-like-this)
-(global-set-key (kbd "C-\"")        'mc/skip-to-next-like-this)
-(global-set-key (kbd "C-:")         'mc/skip-to-previous-like-this)
-
-;; setting up which key
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
 
 ;; org-mode
 (defun org-mode-setup ()
@@ -183,13 +119,13 @@
 (setq org-modules
       '(org-crypt
         org-habit
-        org-bookmark))
+        org-bookmark
+        org-eshell
+        ))
 
 (use-package org
   :hook (org-mode . org-mode-setup)
-  :config
-  (setq org-ellipsis " ▾")
-  (setq org-hide-emphasis-markers t))
+  :config (setq org-ellipsis " ▾"))
 
 (use-package org-bullets
   :after org
@@ -197,106 +133,144 @@
   :custom
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
-;; Replace list hyphen with dot
-(font-lock-add-keywords 'org-mode
-                        '(("^ *\\([-]\\) "
-                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-;; org-roam
+;; setting up org-roam
 (use-package org-roam
   :ensure t
   :init
   (setq org-roam-v2-ack t)
   :custom
   (org-roam-directory "~/Notes/org-roam-notes")
+  (org-roam-completion-everywhere t)
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n i" . org-roam-node-insert)
          :map org-mode-map
-         ("C-M-/" . completion-at-point))
+         ("C-M-i"    . completion-at-point))
   :config
   (org-roam-setup))
 
-(setq find-file-visit-truename t)	;; force emacs to always resolve symlinks
+(setq find-file-visit-truename t)                   ;; force emacs to always resolve symlinks (performace cost)
 
-;; over-ride current behavior of org-roam notes search to be case-insensitive
+;; over-ride the behaviour of org-roam note search to be case-insensitive
 (defun case-insensitive-org-roam-node-read (orig-fn &rest args)
   (let ((completion-ignore-case t))
     (apply orig-fn args)))
 
 (advice-add 'org-roam-node-read :around #'case-insensitive-org-roam-node-read)
 
-;; enable db-autosync
+;; enable database autosync
 (org-roam-db-autosync-mode 1)
+
+;; multiple-cursors
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->")         'mc/mark-next-like-this)
+(global-set-key (kbd "C-<")         'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<")     'mc/mark-all-like-this)
+(global-set-key (kbd "C-\"")        'mc/skip-to-next-like-this)
+(global-set-key (kbd "C-:")         'mc/skip-to-previous-like-this)
 
 ;; auto-completion
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 
-;; dired settings
-(use-package all-the-icons-dired)
+;; dired - list directories first
+(setq dired-listing-switches "-agho --group-directories-first"
+      dired-omit-files "^\\.[^.].*"
+      dired-omit-verbose nil
+      dired-hide-details-hide-symlink-targets nil
+      delete-by-moving-to-trash t)
 
-(use-package dired
-  :ensure nil
-  :defer 1
-  :commands (dired dired-jump)
-  :config
-  (setq dired-listing-switches "-agho --group-directories-first"
-        dired-omit-files "^\\.[^.].*"
-        dired-omit-verbose nil
-        dired-hide-details-hide-symlink-targets nil
-        delete-by-moving-to-trash t)
-
-  (autoload 'dired-omit-mode "dired-x"))
+(autoload 'dired-omit-mode "dired-x")
 
 ;; move-text
 (require 'move-text)
 (global-set-key (kbd "M-p") 'move-text-up)
 (global-set-key (kbd "M-n") 'move-text-down)
 
-;; move between windows uisng Shift + Arrow keys
-(when (fboundp 'windmove-default-keybindings)
-  (windmove-default-keybindings))
+;; move around windows using SHIFT + ARROW keys
+;; (when (fboundp 'windmove-default-keybindings)
+;;   (windmove-default-keybindings))
 
-;; use spaces instead of tabs for consistency across all IDEs
-(setq-default tab-width 4)
+;; will display the funtion def in the menu bar
+(add-hook 'my-mode-hook 'imenu-add-menubar-index)
+(global-set-key (kbd "C-S-f") 'imenu)
+
+;; auto-delete hightlighted text
+(delete-selection-mode 1)
+
+;; use spaces instead of tabs / ensures uniformity among various platforms
 (setq-default indent-tabs-mode nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" "ba4ab079778624e2eadbdc5d9345e6ada531dc3febeb24d257e6d31d5ed02577" default))
+ '(package-selected-packages
+   '(multiple-cursors god-mode zenburn-theme org-roam-ui gruber-darker-theme tree-sitter-langs tree-sitter all-the-icons-dired markdown-mode hydra move-text company)))
 
-;; tree-sitter setup
+
+;; copy from the line above
+(autoload 'copy-from-above-command "misc"
+  "Copy characters from previous nonblank line, starting just above point.
+
+  \(fn &optional arg)"
+  'interactive)
+
+(global-set-key [up] 'copy-from-above-command) ;; up arrow key will copy the rest of the line forward
+
+(global-set-key [down] (lambda ()	;; now the down arrow key will copy from the line above
+                         (interactive)
+                         (forward-line 1)
+                         (open-line 1)
+                         (copy-from-above-command)))
+
+(global-set-key [right] (lambda ()	;; right arrow key will copy one character from above
+                          (interactive)
+                          (copy-from-above-command 1)))
+
+(global-set-key [left] (lambda ()	;; left arrow key will copy from above, backwards!!
+                         (interactive)
+                         (copy-from-above-command -1)
+                         (forward-char -1)
+                         (delete-char -1)))
+
+;; tree-sitter configuration
 (require 'tree-sitter)
 (require 'tree-sitter-langs)
+(global-tree-sitter-mode)
 
-;; lsp config
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :hook ((c-mode . lsp-deferred)
-         (bash-ts-mode . lsp-deferred))
-  :commands (lsp lsp-deferred))
+;; automatic encryption and decryption for gpg encrypted files
+(require 'epa-file)
+(epa-file-enable)
 
-;; addtional lsp configs (if lsp performace degrades)
-;; (setq read-process-output-max (* 1024 1024))
-;; (setq gc-cons-threshold 100000000)
+;; allowing for password prompt in minibuffer
+(setq epa-pinentry-mode 'loopback)
 
-;; enable auto scrolling in the compile and re-compile buffers
-(use-package compile
-  :custom
-  (compilation-scroll-output t))
+;; run-cmd from within EMACS
+(defun run-cmdexe ()
+      (interactive)
+      (let ((shell-file-name "cmd.exe"))
+        (shell "*cmd.exe*")))
 
-(defun auto-recompile-buffer ()
-  (interactive)
-  (if (member #'recompile after-save-hook)
-      (remove-hook 'after-save-hook #'recompile t)
-    (add-hook 'after-save-hook #'recompile nil t)))
+;; use windows clipboard
+(defun copy-selected-text (start end)
+  (interactive "r")
+    (if (use-region-p)
+        (let ((text (buffer-substring-no-properties start end)))
+          (shell-command (concat "echo '" text "' | clip.exe")))))
 
-;; sinpppets
-(use-package yasnippet
-  :hook (prog-mode . yas-minor-mode)
-  :config
-  (yas-reload-all))
+;; WSL-specific setup
+(when (and (eq system-type 'gnu/linux)
+           (getenv "WSLENV"))
 
-;; vterm (not emacs implement terminal, but slightly better)
-(use-package vterm
-  :commands vterm
-  :config
-  (setq vterm-max-scrollback 10000))
+  ;; Teach Emacs how to open links in your default Windows browser (firefox)
+  (let ((cmd-exe "/mnt/c/Windows/System32/cmd.exe")
+        (cmd-args '("/c" "start")))
+    (when (file-exists-p cmd-exe)
+      (setq browse-url-generic-program  cmd-exe
+            browse-url-generic-args     cmd-args
+            browse-url-browser-function 'browse-url-generic
+            search-web-default-browser 'browse-url-generic))))
