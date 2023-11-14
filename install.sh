424e3970ea14181f99f0e9c120db09804c38ce39
rizwan ahmed <rzwn.hmd@gmail.com>
2023-11-14 21:31:59 +0100

    zsh_config, install.sh

--- install.sh
+++ install.sh
@@ -17,14 +17,14 @@ useradd -m -g users -G wheel,storage,power,video,audio,input riz
 # EDITOR=emacs visudo
 # %wheel ALL=(ALL) NOPASSWD: ALL    # uncomement this line
 su riz
-sudo pacman -Sy xdg-user-dirs git pulseaudio xdg-user-dirs-update pulseaudio pavucontrol openssh iw wpa_supplicant network-manager-applet bluez bluez-utils blueman xorg-server xorg-apps xorg-xinit xclip xdotool xorg-drivers i3 picom ttc-iosevka zsh alacritty dmenu rofi rofi-emoji rofi-calc sxiv ueberzug firefox mpv firefox mpv zathura zathura-pdf-mupdf tldr fzf tar gzip htop neofetch fd ripgrep bat lsd tree-sitter tree-sitter-cli maim codespell go luarocks ruby rubygems composer php nodejs npm yarn python python-pip jre-openjdk jdk-openjdk julia wget curl qrencode zbar
+sudo pacman -Sy xdg-user-dirs git pulseaudio xdg-user-dirs-update pulseaudio pavucontrol openssh iw wpa_supplicant network-manager-applet bluez bluez-utils blueman xorg-server xorg-apps xorg-xinit xclip xdotool xorg-drivers i3 picom ttc-iosevka zsh alacritty dmenu rofi rofi-emoji rofi-calc sxiv ueberzug firefox mpv firefox mpv zathura zathura-pdf-mupdf tldr fzf tar gzip htop neofetch fd ripgrep bat lsd tree-sitter tree-sitter-cli maim codespell go luarocks ruby rubygems composer php nodejs npm yarn python python-pip jre-openjdk jdk-openjdk julia wget curl qrencode zbar texlive-core texlive-latexextra latexmk texlive-bibtexextra texlive-fontsextra texlive-science texlive-publishers gnupg pass
 cd ~/Downloads
 mkdir aur
 cd aur
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay -S pa-applet-git
+yay -S pa-applet-git git-remote-gcrypt
 sudo systemctl enable sshd
 sudo systemctl enable dhcpcd
 sudo systemctl enable bluetooth
