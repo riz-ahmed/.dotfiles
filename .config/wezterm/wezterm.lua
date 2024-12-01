881560f02f2eb2601c9d170a9366462eae4bcb0c
rizwan <rzwn.hmd@gmail.com>
2024-12-01 10:22:47 +0100

    add README.md

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,90 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.font = wezterm.font("Iosevka NF")
+config.font_size = 16.0
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+-- config.font = wezterm.font_with_fallback({
+-- 	"Menlo",
+-- })
+config.leader = { key = "a", mods = "CMD", timeout_milliseconds = 5000 }
+local act = wezterm.action
+config.keys = {
+	{
+		mods = "LEADER",
+		key = "%",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = '"',
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "z",
+		mods = "LEADER",
+		action = act.TogglePaneZoomState,
+	},
+	{
+		key = "x",
+		mods = "LEADER",
+		action = act.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = act.CloseCurrentTab({ confirm = true }),
+	},
+	{
+		key = "[",
+		mods = "LEADER",
+		action = act.ActivateCopyMode,
+	},
+	{
+		key = "2",
+		mods = "LEADER",
+		action = act.SpawnWindow,
+	},
+	{
+		mods = "LEADER",
+		key = "c",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "p",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		mods = "LEADER",
+		key = "n",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "LEADER",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+}
+-- and finally, return the configuration to wezterm
+return config
---
+++ README.md
@@ -0,0 +1,21 @@
+# dotfiles
+
+- Just my dotfiles with my personal preference on setting up Arch Linux with all the apps set up to my taste
+- Copied stuff from here and there
+
+## Requirements
+
+- Emacs (29 or higher)
+- git
+- tmux
+- NVIM
+- wezterm
+- zathura
+- zathura-pdf-mupdf
+- zsh
+
+* Quick Start
+
+```console
+git clone git@github.com:riz-ahmed/.dotfiles.git ./
+```
